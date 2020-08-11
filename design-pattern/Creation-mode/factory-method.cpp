169d5807d33337b3779a5e56999148e641fb3431
dmaner <2663515256@qq.com>
2020-08-11 11:03:45 +0800

    add design pattern implemented by c++

---
+++ design-pattern/Creation-mode/factory-method.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Product {
+ public:
+  virtual ~Product() {}
+  virtual void GetType() = 0;
+};
+
+class Factory {
+ public:
+  virtual ~Factory() {}
+  virtual Product* CreateA() const = 0;
+  virtual Product* CreateB() const = 0;
+  virtual void RemoveProduct(Product*) = 0;
+};
+
+class ProductA : public Product {
+ public:
+  ~ProductA() {}
+  void GetType() { cout << "Type A" << endl; }
+};
+
+class ProductB : public Product {
+ public:
+  ~ProductB() {}
+  void GetType() { cout << "Type B" << endl; }
+};
+
+class ConcreteFactory : public Factory {
+ public:
+  ~ConcreteFactory() {}
+  Product* CreateA() const { return new ProductA(); }
+  Product* CreateB() const { return new ProductB(); }
+  void RemoveProduct(Product* p) { delete p; }
+};
+
+int main(int argc, char const* argv[]) {
+  Factory* factory = new ConcreteFactory();
+  Product* p = factory->CreateA();
+  p->GetType();
+  factory->RemoveProduct(p);
+  p = factory->CreateB();
+  p->GetType();
+  factory->RemoveProduct(p);
+  delete factory;
+  return 0;
+}
